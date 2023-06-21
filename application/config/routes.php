38118363b9caf9ae886d62aacb9cc368cf18b33b
Daniel Vazquez <danie.vazquez@utpuebla.edu.mx>
2023-06-21 09:55:15 -0600

    Implementacion de Modelo, Controladores y vistas

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['eliminar_categoria/(:num)']    =   'Productos/eliminarCategoria';
+$route['actualizar_categoria/(:num)']    =   'Productos/actualizarCategoria';
